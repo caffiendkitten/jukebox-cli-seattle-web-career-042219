ae182fd04d8d7cd7c1b3cbf66cbb3603106e830f
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-04-10 01:16:18 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -20,7 +20,7 @@ end
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,13 +33,28 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  help
+  puts "Please enter a command:"
+  command = gets.chomp.downcase
+
+  case command
+  when command = 'help'
+    help
+  when command = 'play'
+    play(songs)
+  when command = 'list'
+    list(songs)
+  when command = 'exit'
+    exit
+  else
+    puts "Please enter a command:"
+  end
 end
